6b5d0e2fa44894194dc85b1681ca673c2204be00
Lukas Åkefeldt <lukas.akefeldt@gmail.com>
2025-01-04 13:51:20 +0100

    sticky scroll

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 function toggleSidebar() {
     const sidebar = document.getElementById("sidebar");
     const burger = document.getElementById("burger");
-  
-    // Toggle the 'open' class on both the sidebar and the burger
+    
+    // Toggle .open on both sidebar and burger
     sidebar.classList.toggle("open");
     burger.classList.toggle("open");
   }
